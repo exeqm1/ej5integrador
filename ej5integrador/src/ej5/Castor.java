41b16043ab1a20d457359ad3cf2b356ea2dee64f
THIAGOCRR <crn700thiago@gmail.com>
2025-08-30 16:29:41 -0300

    Update Castor.java

--- ej5integrador/src/ej5/Castor.java
+++ ej5integrador/src/ej5/Castor.java
@@ -4,9 +4,14 @@ package ej5;
 public class Castor {
     private int cola;
     private int velocidad;      // (hasta 8km/h. ingresar por teclado)
+
+      public Castor(int cola, int velocidad) {
+        this.cola = cola;
+        this.velocidad = velocidad;
+    }
     
-    public int nadar(){
-        return velocidad;
+    public void nadar(){
+       System.out.println("La velocidad es: "+velocidad); 
 
     }    
     public void tocarGuitarra(){
